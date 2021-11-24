f6f6f7e08bdbb63905655f4d0725eb5087be3d8e
Diana Obiedkova <missdee31102001@gmail.com>
2021-11-24 23:38:17 +0200

    position class (will be used in several objects)

---
+++ Agar.io/Agar.io/Models/Position.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agar.io.Models
+{
+    public class Position
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public override int GetHashCode()
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 3049; // Start value (prime number).
+
+                // Suitable nullity checks etc, of course :)
+                hash = hash * 5039 + X.GetHashCode();
+                hash = hash * 883 + Y.GetHashCode();
+                return hash;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = (Position)obj;
+
+            if (other == null)
+                return false;
+
+            return X == other.X && Y == other.Y;
+        }
+    }
+}
