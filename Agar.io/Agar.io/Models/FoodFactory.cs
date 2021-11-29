00b249b366ae1e5e3c8cc05d03f2be23ff749b26
Diana Obiedkova <missdee31102001@gmail.com>
2021-11-30 00:33:55 +0200

    FoodFactory

---
+++ Agar.io/Agar.io/Models/FoodFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agar.io.Models
+{
+    public class FoodFactory
+    {
+        private long count = -1;
+        private readonly int maxWidth, maxHeight;
+        readonly Random ran = new Random();
+        public FoodFactory(int w, int h)
+        {
+            maxHeight = h;
+            maxWidth = w;
+        }
+
+        public Food Create()
+        {
+            count++;
+
+            return new Food(count.ToString(), "Food " + count.ToString())
+            {
+                Location = new Position
+                {
+                    X = ran.Next(maxWidth),
+                    Y = ran.Next(maxHeight)
+                },
+                Weight = ran.Next(1, 5)
+            };
+        }
+    }
+}
