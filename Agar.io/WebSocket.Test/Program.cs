74fcc9242cda57d992d9dbe345f2aa21652d3919
Diana Obiedkova <missdee31102001@gmail.com>
2021-12-02 01:25:01 +0200

    WebSocket Tests

--- Agar.io/WebSocket.Test/Program.cs
+++ Agar.io/WebSocket.Test/Program.cs
@@ -1,70 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace Agar.IO.Client
+namespace WebSocket.Test
 {
-    public class Startup
+    class Program
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            //app.UseAuthorization();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapControllerRoute(
-            //        name: "default",
-            //        pattern: "{controller=Home}/{action=Index}/{id?}");
-            //});
-
-            WebSocketTest();
-        }
-
-        private async Task WebSocketTest()
+        static async Task Main(string[] args)
         {
             Console.WriteLine("press enter to continue...");
-            //Console.ReadLine();
+            Console.ReadLine();
 
-            using(ClientWebSocket client = new ClientWebSocket())
+            using (ClientWebSocket client = new ClientWebSocket())
             {
                 Uri serviceUri = new Uri("ws://localhost:5000/send");
                 var cTs = new CancellationTokenSource();
