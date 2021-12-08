c5a5f1c29dac28705ed01ff0ad57e963cf066b53
Diana Obiedkova <missdee31102001@gmail.com>
2021-12-09 01:31:14 +0200

    players models in front

---
+++ Agar.io/Agar.IO.Client/wwwroot/js/game.js
@@ -0,0 +1,62 @@
+﻿const UP = 0
+const RIGHT = 1
+const DOWN = 2
+const LEFT = 3
+
+var currentPlayer = null;
+var playersList = new PlayersList();
+
+document.onkeydown = function (e) {
+    switch (e.key) {
+        case "ArrowUp":
+            currentPlayer.dirrection = UP
+            break
+        case "ArrowDown":
+            currentPlayer.dirrection = DOWN
+            break
+        case "ArrowLeft":
+            currentPlayer.dirrection = LEFT
+            break
+        case "ArrowRight":
+            currentPlayer.dirrection = RIGHT
+            break
+    }
+}
+
+function move() {
+    
+}
+
+function onMessage(e) {
+    let message = JSON.parse(e.data);
+    let player = new Player(message.person, socket);
+
+    switch (e.eventType) {
+        case "SpawnMyself":
+            currentPlayer = player;
+            playersList.add(player);
+            break;
+        case "Spawn":
+            playersList.add(player);
+            break;
+        case "Die":
+            playersList.remove(person);
+            break;
+        case "CoordsChange":
+            playersList.change(person);
+            break;
+        case "SizeChange":
+            playersList.resize(person);
+            break;
+        default:
+            console.log(message);
+            break;
+        
+    }
+
+    update();
+}
+
+function update() {
+
+}
---
+++ Agar.io/Agar.IO.Client/wwwroot/js/playersList.js
@@ -0,0 +1,24 @@
+﻿class PlayersList {
+    constructor() {
+        this.persons = []
+    }
+
+    add(person) {
+        this.persons.push(person)
+    }
+
+    remove(person) {
+        let index = this.getArrayIndex(person)
+        this.persons.splice(index, 1)
+    }
+
+    change(person) {
+        let index = this.getArrayIndex(person)
+        this.persons[index].position = person.position
+    }
+
+    resize(person) {
+        let index = this.getArrayIndex(person)
+        this.persons[index].size = person.size
+    }
+}
